35ab825949341736951aa09dc2f882015920b359
Francesco Calcavecchia <f18771@eon.com>
2022-09-20 16:06:26 +0200

    make some git utils function private and reduce tests accordingly to only public ones

--- gto/git_utils.py
+++ gto/git_utils.py
@@ -16,7 +16,7 @@ def git_clone_remote_repo(f: Callable):
         kwargs = _turn_args_into_kwargs(args, kwargs)
 
         # noinspection PyTypeChecker
-        if isinstance(kwargs["repo"], str) and is_url_of_remote_repo(
+        if isinstance(kwargs["repo"], str) and _is_url_of_remote_repo(
             repo=kwargs["repo"]
         ):
             try:
@@ -44,28 +44,27 @@ def git_clone_remote_repo(f: Callable):
         kwargs.update(kwargs_complement)
         return kwargs
 
+    def _is_url_of_remote_repo(repo: str) -> bool:
+        if remote_git_repo_regex.fullmatch(repo) is not None:
+            logging.debug("%s recognized as remote git repo", repo)
+            return True
+
+        logging.debug("%s NOT recognized as remote git repo", repo)
+        return False
+
     return wrapped_f
-
-
-def is_url_of_remote_repo(repo: str) -> bool:
-    if remote_git_repo_regex.fullmatch(repo) is not None:
-        logging.debug("%s recognized as remote git repo", repo)
-        return True
-
-    logging.debug("%s NOT recognized as remote git repo", repo)
-    return False
 
 
 @contextmanager
 def cloned_git_repo(repo: str):
     tmp_dir = TemporaryDirectory()
     logging.debug("create temporary directory %s", tmp_dir)
-    git_clone(repo=repo, dir=tmp_dir.name)
+    _git_clone(repo=repo, dir=tmp_dir.name)
     yield tmp_dir.name
     logging.debug("delete temporary directory %s", tmp_dir)
     tmp_dir.cleanup()
 
 
-def git_clone(repo: str, dir: str) -> None:
+def _git_clone(repo: str, dir: str) -> None:
     logging.debug("clone %s in directory %s", repo, dir)
     Repo.clone_from(url=repo, to_path=dir)
--- tests/test_git_utils.py
+++ tests/test_git_utils.py
@@ -7,47 +7,52 @@ import pytest
 from git import Repo
 
 import tests.resources
-from gto.git_utils import (
-    git_clone,
-    git_clone_remote_repo,
-    is_url_of_remote_repo,
-)
+from gto.git_utils import cloned_git_repo, git_clone_remote_repo
 from tests.skip_presets import (
     only_for_windows_py_lt_3_8,
     skip_for_windows_py_lt_3_9,
 )
 
 
-def test_git_clone_remote_repo_if_repo_is_a_meaningless_string_then_leave_it_unchanged():
-    assert_f_called_with_repo_return_repo_itself(repo="meaningless_string")
-
-
-def test_git_clone_remote_repo_if_repo_is_a_local_git_repo_then_leave_it_unchanged(
-    tmp_local_git_repo,
-):
-    assert_f_called_with_repo_return_repo_itself(repo=tmp_local_git_repo)
-
-
 def test_git_clone_remote_repo_if_repo_gitpython_object_then_leave_it_unchanged(
-    tmp_local_git_repo,
+    tmp_gitpython_repo: Repo,
 ):
-    assert_f_called_with_repo_return_repo_itself(repo=Repo(path=tmp_local_git_repo))
+    assert_f_called_with_repo_return_repo_itself(repo=tmp_gitpython_repo)
+
+
+@pytest.mark.parametrize(
+    "repo",
+    (
+        "/local/path",
+        "local/path",
+        ".",
+        "meaningless_string",
+    ),
+)
+def test_git_clone_remote_repo_if_repo_is_string_and_invalid_remote_url_then_leave_repo_as_it_is(
+    repo: str,
+):
+    assert_f_called_with_repo_return_repo_itself(repo=repo)
 
 
 @skip_for_windows_py_lt_3_9
-def test_git_clone_remote_repo_if_repo_is_remote_url_then_clone_and_set_repo_to_its_local_path():
-    with patch("gto.git_utils.git_clone") as mocked_git_clone:
-        mocked_git_clone.side_effect = git_clone
-        local_repo = decorated_func(
-            repo=tests.resources.SAMPLE_HTTP_REMOTE_REPO, spam=0, jam=3
-        )
-        mocked_git_clone.assert_called_once_with(
-            repo=tests.resources.SAMPLE_HTTP_REMOTE_REPO, dir=local_repo
-        )
+@pytest.mark.parametrize(
+    "repo",
+    (
+        tests.resources.SAMPLE_HTTP_REMOTE_REPO,
+        tests.resources.SAMPLE_HTTP_REMOTE_REPO_WITHOUT_DOT_GIT_SUFFIX,
+    ),
+)
+def test_git_clone_remote_repo_if_repo_is_string_and_valid_remote_url_then_clone_and_set_repo_to_its_local_path(
+    repo,
+):
+    with patch("gto.git_utils.cloned_git_repo") as mock_cloned_git_repo:
+        decorated_func(repo=repo, spam=0, jam=3)
+        mock_cloned_git_repo.assert_called_once_with(repo=repo)
 
 
 @only_for_windows_py_lt_3_8
-def test_if_repo_is_remote_url_and_windows_os_error_then_hint_win_with_py_lt_3_9_may_be_the_cause():
+def test_git_clone_remote_repo_if_windows_os_error_then_hint_the_cause():
     with pytest.raises(OSError) as e:
         decorated_func(repo=tests.resources.SAMPLE_HTTP_REMOTE_REPO, spam=0, jam=3)
     assert e.type in (NotADirectoryError, PermissionError)
@@ -56,29 +61,6 @@ def test_if_repo_is_remote_url_and_windows_os_error_then_hint_win_with_py_lt_3_9
     assert "< 3.9" in str(e)
 
 
-@pytest.mark.parametrize(
-    "repo",
-    (
-        "/local/path",
-        "/local/path",
-        ".",
-    ),
-)
-def test_is_url_of_remote_repo_if_local_url_then_true(repo: str):
-    assert is_url_of_remote_repo(repo=repo) is False
-
-
-@pytest.mark.parametrize(
-    "repo",
-    (
-        tests.resources.SAMPLE_HTTP_REMOTE_REPO,
-        tests.resources.SAMPLE_HTTP_REMOTE_REPO_WITHOUT_DOT_GIT_SUFFIX,
-    ),
-)
-def test_is_url_of_remote_repo_if_remote_url_then_true(repo: str):
-    assert is_url_of_remote_repo(repo=repo) is True
-
-
 @skip_for_windows_py_lt_3_9
 @pytest.mark.parametrize(
     "repo",
@@ -87,10 +69,15 @@ def test_is_url_of_remote_repo_if_remote_url_then_true(repo: str):
         tests.resources.SAMPLE_HTTP_REMOTE_REPO_WITHOUT_DOT_GIT_SUFFIX,
     ),
 )
-def test_git_clone_if_valid_remote_then_clone(repo: str):
-    with TemporaryDirectory() as tmp_repo_dir:
-        git_clone(repo=repo, dir=tmp_repo_dir)
-        assert_dir_contain_git_repo(dir=tmp_repo_dir)
+def test_cloned_git_repo_if_valid_remote_then_clone(repo: str):
+    with cloned_git_repo(repo=repo) as repo_dir:
+        assert_dir_contain_git_repo(dir=repo_dir)
+
+
+def test_cloned_git_repo_if_invalid_remote_then_raise_exception():
+    with pytest.raises(Exception):
+        with cloned_git_repo(repo="invalid_remote_url"):
+            pass
 
 
 @git_clone_remote_repo
@@ -107,14 +94,13 @@ def assert_f_called_with_repo_return_repo_itself(repo: Union[str, Repo]) -> None
     assert decorated_func(spam=0, jam=3, repo=repo) is repo
 
 
-@pytest.fixture
-def tmp_local_git_repo() -> str:
-    tmp_repo_dir = TemporaryDirectory()  # pylint: disable=consider-using-with
-    Repo.init(path=tmp_repo_dir.name)
-    yield tmp_repo_dir.name
-    tmp_repo_dir.cleanup()
-
-
 def assert_dir_contain_git_repo(dir: str) -> None:
     assert (Path(dir) / ".git").is_dir()
     assert (Path(dir) / ".git/HEAD").is_file()
+
+
+@pytest.fixture
+def tmp_gitpython_repo() -> Repo:
+    tmp_repo_dir = TemporaryDirectory()  # pylint: disable=consider-using-with
+    yield Repo.init(path=tmp_repo_dir.name)
+    tmp_repo_dir.cleanup()
